2e4e6aa40261fd24be93299ec8d3a2e1221a7042
MikiUA <106928450+MikiUA@users.noreply.github.com>
2023-02-23 19:48:39 +0200

    played with router

--- src/hw-05-movies/App.js
+++ src/hw-05-movies/App.js
@@ -1,15 +1,25 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Link, Navigate, Route, Routes, useLocation } from "react-router-dom"
 import Home from "./components/Home"
 import MovieDetails from "./components/MovieDetails"
 import Movies from "./components/Movies"
 
 export default function App() {
     const hw5path='/hw-05-movies';
+    const location=useLocation();
+
+    function Logloc({location}){
+        console.log(location);
+        return <></>
+    }
+    Logloc({location});
+
     return (  
     <Routes>
+
         <Route path="/" element={<Home/>}/>
         <Route path="/movies" element={<Movies/>}/>
         <Route path="/movies/:movieID/*" element={<MovieDetails/>}/>
-        <Route path="/*" element={<Navigate to={hw5path}/>}/>
+        {/* <Route path="/*" element={<Navigate to={hw5path}/>}/> */}
+        <Route path="/*" element={<Link to="/">Link</Link>}/>
     </Routes>)
 }
