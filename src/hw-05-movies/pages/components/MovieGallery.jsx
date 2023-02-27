0b90d449759603bc2a6d309472b0eef513baee26
MikiUA <106928450+MikiUA@users.noreply.github.com>
2023-02-27 18:43:47 +0200

    edited out comments and updated proptypes, moviegallery component

--- src/hw-05-movies/pages/components/MovieGallery.jsx
+++ src/hw-05-movies/pages/components/MovieGallery.jsx
@@ -36,46 +36,12 @@ function MovieGallery({items,isLoading}) {
 MovieGallery.propTypes = {
     items:PropTypes.arrayOf(
         PropTypes.shape({
-            // id:PropTypes.any.isRequired,
-            // webformatURL:PropTypes.string,
-            // largeImageURL:PropTypes.string
+            id:PropTypes.any,
+            original_title:PropTypes.string,
+            poster_path:PropTypes.string
         })
     ),
     isLoading:PropTypes.oneOf(['loading','updating',false])
 }
 
 export default MovieGallery
-
-// import React, { Component } from 'react'
-// import Notification from 'generalComponents/Notification'
-// import {ImageGalleryItem} from './ImageGalleryItem'
-// import { Loader } from './Loader'
-// import PropTypes from 'prop-types'
-// import { Modal } from './Modal'
-// import styles from './styles/ImageGallery.module.css'
-
-
-// export default class ImageGallery extends Component {
-//  
-//   state={
-//     modal:''
-//   }
-//   render() {
-//     if (this.props.isLoading==='loading') return <Loader/>
-//     if (this.props.items.length===0) return <Notification message='Sorry, no images matched your search'/>
-//     return (
-//         <div>
-//         {this.state.modal?<Modal imgSrc={this.state.modal} onAbortF={()=>this.setState({modal:''})}/>:''}
-//         <ul className={styles['ImageGallery']}>
-//             {this.props.items.map((item)=>(
-//                 <ImageGalleryItem key={item.id} item={item} onClickF={(url)=>this.setState({modal:url})}/>
-//             ))}
-//         </ul>
-//         {this.props.isLoading==='updating'?(
-//                 <Loader/>
-//         ):''}
-//     </div>
-//     )
-//   }
-// }
-
